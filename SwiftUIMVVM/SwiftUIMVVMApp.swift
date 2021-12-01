d765cd65eba52aa83a42fda77208d29974ae9c7d
Shireesh Marla <shireesh.marla@swipiicard.com>
2021-12-01 13:43:58 +0000

    Initial Commit

---
+++ SwiftUIMVVM/SwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMApp.swift
+//  SwiftUIMVVM
+//
+//  Created by Shireesh Marla on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
